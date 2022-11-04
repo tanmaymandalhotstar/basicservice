cedb29343315d29ab8103ed38764e85a8a1df495
tanmaymandalhotstar <tanmay.mandal@hotstar.com>
2022-11-04 14:22:01 +0530

    wht

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import com.google.gson.Gson;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.ApiResponse;
@@ -11,11 +13,15 @@ import io.kubernetes.client.util.ClientBuilder;
 import io.kubernetes.client.util.Yaml;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.mashape.unirest.http.*;
 
 import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -25,6 +31,32 @@ public class HelloController {
     public String index() {
         String kuben = System.getenv("KUBERNETES_SERVICE_HOST");
         return "Greetings from Spring Boot! host: "+kuben;
+    }
+
+
+    @PostMapping("/sup")
+    public String sup(@RequestBody Map<String,Object> mystr) throws UnirestException {
+        Map<String, Object> datas = new HashMap<>();
+        datas.put("EndTime", String.valueOf(new Long(1)));
+        datas.put("StartTime", String.valueOf(new Long(2)));
+        datas.put("ScenariosFailed", new Integer(3));
+        datas.put("ScenariosPassed", new Integer(4));
+        datas.put("ScenariosTotal", new Integer(5));
+        datas.put("reportPath", "a");
+        datas.put("reportPathKarate", "b");
+        datas.put("requestId", String.valueOf(new Long(33133863)));
+
+        Gson gson = new Gson();
+        Map<String, Object> map = new HashMap<>();
+        Long x = Long.parseLong((String) mystr.get("munna"));
+        String output = gson.toJson(datas);
+        Unirest.setTimeouts(0, 0);
+        HttpResponse<String> response;
+        response = Unirest.post("http://localhost:8080/" + "add_to_db")
+                .header("Content-Type", "application/json").body(output).asString();
+
+
+        return output;
     }
 
     @GetMapping("/add_job")
